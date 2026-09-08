1e16f7aed8a68994e45969abb8e340158e147b11
muhammedbelloakorede-debug <muhammadbelloakorede@gmail.com>
2026-09-07 23:03:04 -0700

    Initial commit:MyInfo-Assignment

---
+++ intro.js
@@ -0,0 +1,6 @@
+console.log('Muhammed Bello');
+console.log('My first TECHIFIED Assignment');
+console.log('17 years Old');
+console.log('javascript');
+console.log('Backend development');
+console.log('BETECHIFIED');
